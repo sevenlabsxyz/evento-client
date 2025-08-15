7b2ced7a326c7c9255c2ca07f2f8bf3121a9ffe2
Andre Neves <andrerfneves@protonmail.com>
2025-08-15 12:34:16 -0700

    chore: fixes

--- components/ui/silk-lightbox/index.ts
+++ components/ui/silk-lightbox/index.ts
@@ -1,4 +1,4 @@
-export { ExampleLightbox } from './example-lightbox';
-export { Lightbox } from './lightbox';
-export { SilkLightbox } from './silk-lightbox';
-export type { SilkLightboxRef } from './silk-lightbox';
+export { ExampleLightbox } from "./example-lightbox";
+export { Lightbox } from "./lightbox-core";
+export { SilkLightbox } from "./silk-lightbox";
+export type { SilkLightboxRef } from "./silk-lightbox";
