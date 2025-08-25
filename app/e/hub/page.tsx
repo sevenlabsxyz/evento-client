1b657b747963f218129d91dd2d30c612dfcd1761
Leeyah-123 <junaidaaliyah260@gmail.com>
2025-08-25 11:20:08 +0200

    fix: Hub page desktop view

--- app/e/hub/page.tsx
+++ app/e/hub/page.tsx
@@ -52,7 +52,7 @@ export default function HubPage() {
 
   return (
     <>
-      <div className='flex h-full w-full flex-col gap-4 bg-gray-50 px-4 pb-16'>
+      <div className='mx-auto flex h-full w-full flex-col gap-4 bg-gray-50 px-4 pb-16 md:max-w-sm'>
         <div className='pt-4 text-left text-base text-gray-500'>
           Welcome back @{user?.username},
         </div>
